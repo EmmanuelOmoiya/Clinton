f9bd6515dd8138eb3500f459e02de71a445aa2c5
EmmanuelOmoiya <79797604+EmmanuelOmoiya@users.noreply.github.com>
2021-05-25 21:20:35 +0100

    Change in voice personality from female to male.

--- Clinton-file/Clinton(Virtual Assistant).py
+++ Clinton-file/Clinton(Virtual Assistant).py
@@ -36,7 +36,7 @@ print('Loading Clinton......')
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[0].id)
+engine.setProperty('voice',voices[1].id)
 
 
 def speak(audio):
